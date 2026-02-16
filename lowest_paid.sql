02ec528f43cd9251aa913caa35af0eecf6c0cec7
s3achan <s3achan@gmail.com>
2026-02-16 12:40:54 -0600

    Add alternative SQL query for lowest paid employees
    Added an alternative solution to retrieve the lowest paid employees with completed projects.

--- lowest_paid.sql
+++ lowest_paid.sql
@@ -1,1 +1,30 @@
-WITH projects2 AS (\n    SELECT ep.employee_id as employee_id,\n           COUNT(project_id) AS completed_projects \n    FROM projects p \n    LEFT JOIN employee_projects ep\n    ON p.id = ep.project_id\n    WHERE end_date IS NOT NULL \n    GROUP BY ep.employee_id \n    HAVING COUNT(project_id) >= 2 \n)\n\nSELECT employee_id,\n    salary,\n    completed_projects\nFROM employees e JOIN projects2 p \nON e.id = p.employee_id\nORDER BY salary ASC \nLIMIT 3
+WITH projects2 AS (
+    SELECT ep.employee_id as employee_id,
+           COUNT(project_id) AS completed_projects 
+    FROM projects p 
+    LEFT JOIN employee_projects ep
+    ON p.id = ep.project_id
+    WHERE end_date IS NOT NULL 
+    GROUP BY ep.employee_id 
+    HAVING COUNT(project_id) >= 2 
+)
+
+SELECT employee_id,
+    salary,
+    completed_projects
+FROM employees e JOIN projects2 p 
+ON e.id = p.employee_id
+ORDER BY salary ASC 
+LIMIT 3 
+
+---- Other Solution -- 
+
+SELECT  ep.employee_id, e.salary, COUNT(p.id) AS completed_projects
+FROM employee_projects AS ep
+JOIN employees AS e ON e.id = ep.employee_id
+JOIN projects AS p ON  ep.project_id = p.id
+WHERE p.end_date IS NOT NULL
+GROUP BY 1
+HAVING completed_projects > 1
+ORDER BY 2
+LIMIT 3
